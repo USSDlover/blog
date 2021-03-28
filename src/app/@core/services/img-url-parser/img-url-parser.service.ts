d33306479e290303e03ba8f51b769550082630d5
Alireza Razinejad <a.razinejad@gmail.com>
2021-03-28 09:12:35 +0430

    Small prefix to get ready for publishing v0.10.65

--- src/app/@core/services/img-url-parser/img-url-parser.service.ts
+++ src/app/@core/services/img-url-parser/img-url-parser.service.ts
@@ -3,20 +3,20 @@ import {AppConfig} from '@core/config/app.config';
 
 @Injectable()
 export class ImgUrlParserService {
-  protected apiServer = AppConfig.settings.apiServer.base;
+  protected apiServer = AppConfig.settings.apiServer.base + AppConfig.settings.apiServer.meta;
 
   constructor() {
   }
 
   addApiToImageUrl(imageName: string): string {
-    return this.apiServer + '/' + imageName;
+    return this.apiServer + imageName;
   }
 
   parseImageUrl<T>(obj: T, property: string, optionalPath?: string): T {
     if (optionalPath) {
-      obj[`${property}`] = this.apiServer + '/' + optionalPath + '/' + obj[`${property}`];
+      obj[`${property}`] = this.apiServer + optionalPath + obj[`${property}`];
     } else {
-      obj[`${property}`] = this.apiServer + '/' + obj[`${property}`];
+      obj[`${property}`] = this.apiServer + obj[`${property}`];
     }
     return obj;
   }
